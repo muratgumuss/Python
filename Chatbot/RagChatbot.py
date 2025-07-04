25bdc0f3c345fa341dc2ed0b6703efd43e8ec286
Murat Gümüş <muratgumus05@gmail.com>
2025-07-04 19:12:20 +0300

    rag groq

---
+++ Chatbot/RagChatbot.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
