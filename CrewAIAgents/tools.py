3284a8c7568bd27cf092282ab2fc0df553dfbf10
Murat Gümüş <muratgumus05@gmail.com>
2025-08-15 18:52:46 +0300

    crewai agents

---
+++ CrewAIAgents/tools.py
@@ -0,0 +1,6 @@
+from crewai_tools import YoutubeChannelSearchTool
+
+# YouTube kanalı için arama aracını başlat
+youtube_channel_tool = YoutubeChannelSearchTool(
+    youtube_channel_handle="@krishnaik06" 
+)
